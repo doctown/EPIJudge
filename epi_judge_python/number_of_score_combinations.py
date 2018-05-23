2e1fddb99e2795783a9bd916d9b4455f6eb4a4a8
David Ogor <david.ogor@brightbytes.net>
2018-05-23 05:45:17 -0700

    Almost working

--- epi_judge_python/number_of_score_combinations.py
+++ epi_judge_python/number_of_score_combinations.py
@@ -1,6 +1,38 @@
+"""
+    w 2  3  7
+    0 0  0  0
+    1 2  3  7
+    . 4  6  14
+    . 6  9  21
+    .
+    12
+
+score 0 1 2 3 4
+    2 0 2 4 6
+  2,3 0 3 6 9 12
+2,3,7 0 5 10 0
+    1 2  3  7
+    . 4  6  14
+    . 6  9  21
+    .
+    12
+
+    T[i][j] =
+"""
+
+
 def num_combinations_for_final_score(final_score, individual_play_scores):
-    # Implement this placeholder.
-    return 0
+    T = [[0] * (final_score + 1) for _ in individual_play_scores]
+    for i in range(len(individual_play_scores)):
+        T[i][0] = 1
+
+    print(T)
+    for i in range(len(individual_play_scores)):
+        for score in range(1, final_score + 1):
+            T[i][score] = sum(map(lambda val: T[i][score - val] if score - val >= 0 else 0, individual_play_scores[:i + 1]))
+
+    print(T)
+    return T[len(individual_play_scores) - 1][score]
 
 
 from sys import exit
