38a7b8f85dbc16386a9ac9c8a9f36022cdf82465
David Ogor <david.ogor@brightbytes.net>
2018-05-23 05:44:18 -0700

    Completed with help from book

--- epi_judge_python/is_string_in_matrix.py
+++ epi_judge_python/is_string_in_matrix.py
@@ -1,6 +1,27 @@
 def is_pattern_contained_in_grid(grid, S):
     # Implement this placeholder.
-    return True
+# k, -> the length of the sequence, T[i][j][k] = T[i-1][j][k -1] or T[i+1][j][k-1] or ... and T[i][j] == grid[k]
+    # T = [[[True] * (len(grid) + 1) for _ in range(len(grid[0]) + 1)] for _ in range(S) + 1]
+
+    # for r in range(1, len(grid) + 1):
+    #     for c in range(1, len(grid) + 1):
+    #         for k in range(S):
+    #             T[r][c][k] = grid[k] == T[r][c] and (T[r-1][c][k-1] or T[r+1][c][k-1] or T[r][c-1][k-1] or T[r][c+1][k-1])
+    bad_path = set()
+
+    def is_pattern_contain_to_k(r, c, k):
+        if k == len(S):
+            return True
+
+        if 0 <= r < len(grid) and 0 <= c < len(grid[0]) and (r, c, k) not in bad_path \
+                and grid[r][c] == S[k] \
+                and any([is_pattern_contain_to_k(next_r, next_c, k + 1) for next_r, next_c in ((r + 1, c), (r - 1, c), (r, c - 1), (r, c + 1))]):
+                    return True
+
+        bad_path.add((r, c, k))
+        return False
+
+    return any([is_pattern_contain_to_k(r, c, 0) for r in range(len(grid)) for c in range(len(grid[0]))])
 
 
 from sys import exit
