7005264eaba90734b333d35566e8b81efaeb53c7
David Ogor <david.ogor@brightbytes.net>
2018-06-02 16:09:21 -0700

    Completed trapped water w/ help

--- epi_judge_python/max_trapped_water.py
+++ epi_judge_python/max_trapped_water.py
@@ -1,6 +1,24 @@
 def get_max_trapped_water(heights):
-    # Implement this placeholder.
-    return 0
+    start_index, end_index = (0, len(heights) - 1)
+    max_water_depth = float('-inf')
+
+    while start_index < end_index:
+        min_level = min(heights[start_index], heights[end_index])
+        depth = calculate_depth(start_index, end_index, min_level)
+
+        if depth > max_water_depth:
+            max_water_depth = depth
+        if heights[start_index] <= heights[end_index]:
+            start_index += 1
+        else:
+            end_index -= 1
+
+    return max_water_depth
+
+
+def calculate_depth(start, end, level):
+    return abs(end - start) * level
+
 
 
 from sys import exit
